fe697e7d7830734f24dfed26425eea48998291d6
Alex <alex-w1991@hotmail.co.uk>
2021-10-21 20:22:35 +0100

    Enable migations for project

---
+++ WolfpackBot.Data/Migrations/20211021185946_InitialCreate.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WolfpackBot.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    ShortName = table.Column<string>(type: "TEXT", nullable: true),
+                    GuildId = table.Column<string>(type: "TEXT", nullable: true),
+                    ChannelId = table.Column<string>(type: "TEXT", nullable: true),
+                    RoleId = table.Column<string>(type: "TEXT", nullable: true),
+                    Closed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Capacity = table.Column<int>(type: "INTEGER", nullable: true),
+                    MessageId = table.Column<string>(type: "TEXT", nullable: true),
+                    Round = table.Column<int>(type: "INTEGER", nullable: true),
+                    LastRoundDate = table.Column<string>(type: "TEXT", nullable: true),
+                    LastRoundTrack = table.Column<string>(type: "TEXT", nullable: true),
+                    StandingsMessageId = table.Column<ulong>(type: "INTEGER", nullable: true),
+                    NextRoundDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    NextRoundTrack = table.Column<string>(type: "TEXT", nullable: true),
+                    NextTrackMessageId = table.Column<ulong>(type: "INTEGER", nullable: true),
+                    TwitterMessage = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaderboardModerators",
+                columns: table => new
+                {
+                    RoleId = table.Column<string>(type: "TEXT", nullable: false),
+                    GuildId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaderboardModerators", x => new { x.GuildId, x.RoleId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Leaderboards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GuildId = table.Column<string>(type: "TEXT", nullable: true),
+                    ChannelId = table.Column<string>(type: "TEXT", nullable: true),
+                    Game = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    StartDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Active = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leaderboards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChampionshipResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Pos = table.Column<int>(type: "INTEGER", nullable: false),
+                    Driver = table.Column<string>(type: "TEXT", nullable: true),
+                    Number = table.Column<string>(type: "TEXT", nullable: true),
+                    Car = table.Column<string>(type: "TEXT", nullable: true),
+                    Points = table.Column<string>(type: "TEXT", nullable: true),
+                    Diff = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChampionshipResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChampionshipResults_Event_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventAliasMapping",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Alias = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventAliasMapping", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventAliasMapping_Event_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventSignup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventSignup", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventSignup_Event_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExcelSheetEventMapping",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Sheetname = table.Column<string>(type: "TEXT", nullable: true),
+                    IsRoundsSheet = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExcelSheetEventMapping", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExcelSheetEventMapping_Event_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaderboardEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LeaderboardId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubmittedById = table.Column<string>(type: "TEXT", nullable: true),
+                    ProofUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    Time = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    SubmittedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Invalidated = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaderboardEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaderboardEntries_Leaderboards_LeaderboardId",
+                        column: x => x.LeaderboardId,
+                        principalTable: "Leaderboards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChampionshipResults_EventId",
+                table: "ChampionshipResults",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventAliasMapping_EventId",
+                table: "EventAliasMapping",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventSignup_EventId",
+                table: "EventSignup",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExcelSheetEventMapping_EventId",
+                table: "ExcelSheetEventMapping",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaderboardEntries_LeaderboardId",
+                table: "LeaderboardEntries",
+                column: "LeaderboardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChampionshipResults");
+
+            migrationBuilder.DropTable(
+                name: "EventAliasMapping");
+
+            migrationBuilder.DropTable(
+                name: "EventSignup");
+
+            migrationBuilder.DropTable(
+                name: "ExcelSheetEventMapping");
+
+            migrationBuilder.DropTable(
+                name: "LeaderboardEntries");
+
+            migrationBuilder.DropTable(
+                name: "LeaderboardModerators");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "Leaderboards");
+        }
+    }
+}
